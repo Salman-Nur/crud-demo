73a41783c3182f98876aff2222e26e54bfc2d9b2
Salman Nur <salman.installer@gmail.com>
2024-01-11 09:26:38 +0600

    1st

--- src/Exam1/Exam1.Web/Areas/Admin/Models/StudentSearch.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Exam1.Web.Areas.Admin.Models
-{
-    public class StudentSearch
-    {
-        public string Name { get; set; }
-        public uint StudentFeesFrom { get; set; }
-        public uint StudentFeesTo { get; set; }
-    }
-}
--- src/Exam1/Exam1.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Exam1.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
